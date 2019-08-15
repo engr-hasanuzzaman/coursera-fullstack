1c03333afdef8ca0642de75ae5c6be32dd9dc858
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2019-08-16 01:34:21 +0600

    Fixing component code

--- confusion/src/components/menu.js
+++ confusion/src/components/menu.js
@@ -51,14 +51,15 @@ class Menu extends Component{
               <Media object src={dish.image} alt={dish.name} />
             </Media>
 
-            <Media body className="mt-5">
-              <Media heading={dish.name}></Media>
+            <Media body className="ml-5">
+              <Media heading> {dish.name} </Media>
               <p>{dish.description}</p>
             </Media>
           </Media>
         </div>
       );
     });
+    
     return (
       <div className="container">
         <div className="row">
