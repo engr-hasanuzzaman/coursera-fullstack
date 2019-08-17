050197fd814f643e7a41109739027f308fb3b30f
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2019-08-17 20:01:05 +0600

    Change datetime format of comment

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -20,7 +20,7 @@ class DishDetail extends Component{
             return (
               <li key={comment.id}>
                 <p>{comment.comment}</p>
-                <p>-- {comments[0].author}, {new Date(comments[0].date).toDateString()}</p>
+                <p>-- {comments[0].author}, { new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date))) }</p>
               </li>
             )
           })
