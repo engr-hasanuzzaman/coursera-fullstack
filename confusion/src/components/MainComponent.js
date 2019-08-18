6f1c7a61c5a45f81829f3b349299391ca751e40b
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2019-08-19 00:32:57 +0600

    Configure basic react router

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -1,10 +1,11 @@
 import React, {Component } from 'react';
-import { Navbar, NavbarBrand} from 'reactstrap';
+import {Switch, Route, Redirect} from 'react-router-dom';
+
 import Menu from './MenuComponents';
-import DishDetail from './DishdetailComponent';
 import Footer from './FooterComponent';
 import Header from './HeaderComponent'
 import {DISHES} from '../shared/dishes'
+import Home from './HomeComponent';
 
 class Main extends Component{
   constructor(props){
@@ -27,14 +28,22 @@ class Main extends Component{
     console.log("Main componentDidMount");
   }
 
+  
   render() {
-    console.log("main component render");
+    const HomePage = () => {
+      return (
+        <Home />
+      );
+    }
+
     return (
       <div>
         <Header/>
-        <Menu dishes={this.state.dishes}
-              onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}/>
+        <Switch>
+          <Route path="/home" component={HomePage}/>
+          <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />}/>
+          <Redirect path="/home" />
+        </Switch>
       <Footer/>
       </div>
     );
