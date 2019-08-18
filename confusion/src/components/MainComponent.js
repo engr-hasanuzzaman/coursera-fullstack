3993a05c93013ed5ea45bd7788cb0fd30280ef51
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2019-08-18 22:33:32 +0600

    Add header and footer

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -2,6 +2,8 @@ import React, {Component } from 'react';
 import { Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './MenuComponents';
 import DishDetail from './DishdetailComponent';
+import Footer from './FooterComponent';
+import Header from './HeaderComponent'
 import {DISHES} from '../shared/dishes'
 
 class Main extends Component{
@@ -29,14 +31,11 @@ class Main extends Component{
     console.log("main component render");
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-          <NavbarBrand href="/">Home</NavbarBrand>
-          </div>
-        </Navbar>
+        <Header/>
         <Menu dishes={this.state.dishes}
               onClick={(dishId) => this.onDishSelect(dishId)} />
         <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}/>
+      <Footer/>
       </div>
     );
   }
