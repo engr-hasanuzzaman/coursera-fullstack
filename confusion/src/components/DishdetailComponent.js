a42bd526e82932d072d114b02c591088c0c8e1a3
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2019-08-23 23:39:28 +0600

    Fix assignme 3

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -47,6 +47,8 @@ class CommentForm extends Component {
                     <option value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
                   </Control.select>
                 </Col>
             </Row>
