e59a3ac7418b0f181fca34857b3082fb7a73980c
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2020-07-31 20:53:39 +0600

    Practice any type in typescript

---
+++ typescript/basic_type.ts
@@ -0,0 +1,9 @@
+// type any means, type checker stop type checking. So you can push anythinkg in any
+// We should avoid using "any"
+const anys = [];
+anys.push(1);
+anys.push("oh no");
+anys.push({ anything: "goes" });
+alert(anys);
+
+// 
