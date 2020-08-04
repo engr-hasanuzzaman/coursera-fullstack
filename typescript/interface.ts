06a35b0066d5556d427a6d1a0f72e1e88ff4e658
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2020-08-04 23:59:37 +0600

    Update

--- typescript/interface.ts
+++ typescript/interface.ts
@@ -6,3 +6,7 @@ interface Person {
 function printPerson(person: Person) {
     console.log(`Name: ${person.name}, age: ${person.age}`);
 }
+
+printPerson({name: 'sumon', age: 24});
+
+// Optional Properties
