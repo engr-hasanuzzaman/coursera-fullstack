894d3eb0015837046c6458132d32c2db2b6bdebe
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2020-08-06 16:29:03 +0600

    Funtions file for keeping notes and example code

---
+++ typescript/functions.ts
@@ -0,0 +1,10 @@
+// declaring function type both params & return are required but
+// during function implementation return type is optional as typscript can guess
+// return type by return statement
+
+// Number of param have to be matched (in js this does not required)
+// In JavaScript, every parameter is optional, and users may leave them off as they see fit.
+
+// It is not possible to pass null to defaut parameter
+
+// f a default-initialized parameter comes before a required parameter, users need to explicitly pass undefined to get the default initialized value.
