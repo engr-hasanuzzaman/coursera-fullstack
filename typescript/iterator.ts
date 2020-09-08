f80291e97f9e588cbf552bfd9552bc63f3bb5dbc
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2020-09-08 23:48:18 +0600

    another example

--- typescript/iterator.ts
+++ typescript/iterator.ts
@@ -9,3 +9,14 @@ for (let i in list) {
 for (let i of list) {
   console.log(i); // "4", "5", "6"
 }
+
+let pets = new Set(["Cat", "Dog", "Hamster"]);
+pets["species"] = "mammals";
+
+for (let pet in pets) {
+  console.log(pet); // "species"
+}
+
+for (let pet of pets) {
+  console.log(pet); // "Cat", "Dog", "Hamster"
+}
