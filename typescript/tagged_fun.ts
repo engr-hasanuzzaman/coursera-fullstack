6589ae859c9e96420a2faa4d1acc0cdf830f29c0
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2020-11-03 12:36:55 +0600

    Experiment taged template literal

---
+++ typescript/tagged_fun.ts
@@ -0,0 +1,10 @@
+const f1 = "First value";
+const f2 = 'Second value';
+const input = `This is ${f1} and we also have the ${f2}`;
+
+function randomTagedFunction(first, ...rest) {
+    console.log("first arg ", first);
+    console.log("rem args is ", rest);
+}
+
+randomTagedFunction(input);
